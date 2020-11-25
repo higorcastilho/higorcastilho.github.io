b1d284a7c0dca6d9810b8faba4990d03fddf357d
Higor Castilho <castilhohf@gmail.com>
2020-11-24 23:48:33 -0300

    refactor: move javascript from index.html to actions.js

--- actions.js
+++ actions.js
@@ -1,10 +1,19 @@
 //modal toggle logic
 
+function myFunction() {
+  var elmnt = document.getElementsByTagName("BODY")[0];
+  var y = elmnt.scrollTop;
+  if ( y > 100) {
+  	document.querySelector(".bg-modal").style.display = "flex"
+  }
+}
+
+window.onload = () => {
+	window.addEventListener("scroll", myFunction)
+}
+
 document.querySelector('.close').addEventListener('click', () => {
 	document.querySelector(".bg-modal").style.display = "none"
 
 	window.removeEventListener("scroll", myFunction)
 })
-
-//show
-
